a2b297c939f52f3d298a2d74753e3a8e59e8136e
baka <baka@gmail.com>
2024-10-29 20:39:42 +0530

    added few lines in the index.js file

--- TestingGitFolder/index.js
+++ TestingGitFolder/index.js
@@ -1,1 +1,6 @@
+
+// If we modify anything in the committed code then it will
+// show as Modified
 console.log("Testing git folder!!");
+const userName = "Brendan Eich";
+console.log(` User Name is  :  ${userName} `);
